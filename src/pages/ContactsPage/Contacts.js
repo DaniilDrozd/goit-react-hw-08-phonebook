f1666c60214f8ed17e8d5b5fd0f6efdd652ca859
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-10-09 15:25:57 +0300

    end

--- src/pages/ContactsPage/Contacts.js
+++ src/pages/ContactsPage/Contacts.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContact } from 'redux/Contacts/operations';
 import ContactList from 'components/ContactList/ContactList';
-import { Filter } from 'components/Filter/Filter';
+import Filter from 'components/Filter/Filter';
 import ContactForm from 'components/ContactForm/ContactForm';
 import { selectLoading, selectError } from 'redux/Contacts/selectors';
 
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -3,7 +3,9 @@ import css from './Home.module.css';
 export default function Home() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Welcome to site</h1>
+      <h1 className={css.title}>
+        On this site you can make your own phonebook.
+      </h1>
     </div>
   );
 }
